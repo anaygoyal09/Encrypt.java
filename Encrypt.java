d41f076fd23de7d6be1b4d5c16cf403c7d0a1e55
Anay Goyal <163686471+anaygoyal09@users.noreply.github.com>
2025-01-13 12:55:18 -0800

    Add files via upload
    This is only for 2 increment, the other code, you can choose how much you want to increment the alpha numeric value.

--- Encrypt.java
+++ Encrypt.java
@@ -5,7 +5,7 @@
  * getString for getting the user input,
    encryptString for encrypting the user input,
    printString for printing the line, and
-   rotateTimes that would contain a loop and call the other methods. 
+   rotate13Times that would contain a loop and call the other methods. 
  */
 import java.util.Scanner;
 public class Encrypt
@@ -13,97 +13,66 @@ public class Encrypt
 	private String Message;
 	private String EncryptedMessage;
 	private int EncryptNumber;
-	private int increment;
-	private String InitialMessage;
-
-
+	
 	public Encrypt ()
 	{
 		Message = new String("");
 		EncryptedMessage = new String("");
-		EncryptNumber = 1;
-
-
+		
 	}
 	public static void main(String [] args)
 	{
 		Encrypt et = new Encrypt();
-		et.rotateTimes();
-		System.out.print("\n\n\n");
+		et.rotate13Times();
 	}
 	public void getString()
 	{
 		Scanner reader = new Scanner(System.in);
-		System.out.print("\n\n\nWelcome to Encrypt.java. Please enter a word, phrase, or sentence.\n\n->  ");
-		Message = reader.nextLine();
-		InitialMessage = Message;
-		System.out.print("\n\n\nWhat increment?\n\n->  ");
-		increment = reader.nextInt();
-		//increment = 2;
+		System.out.print("Welcome to Encrypt.java. Please enter a word, phrase, or sentence.\n\n->  ");
 		
-
+		Message = reader.nextLine();
 	}
+	
 	public void encryptString()
 	{
-		int tempPlace;
-		if (increment<0)
-			increment = increment%26+26;
-		else
-			increment = increment%26;
-		
-		for(int i = 1; i < Message.length()+1; i++)
+		for(int i = 1; i < Message.length(); i++)
 		{
-			
-			if((int)Message.charAt(i-1)>=65&&(int)Message.charAt(i-1)<=90)
+			if((int)Message.charAt(i-1)>=32&&(int)Message.charAt(i-1)<=64||(int)Message.charAt(i-1)>=91&&(int)Message.charAt(i-1)<=96 ||(int)Message.charAt(i-1)>=123&&(int)Message.charAt(i-1)<=126)
 			{
-					
-				tempPlace = (int)Message.charAt(i-1)+ increment;
-				if (tempPlace > 90)
-				{
-					EncryptedMessage += (char)((int)Message.charAt(i-1)+increment-26);
-				}
-				else
-					EncryptedMessage += (char)((int)Message.charAt(i-1)+increment);
-			}
-			else if((int)Message.charAt(i-1)>=97&&(int)Message.charAt(i-1)<=122)
-			{
-				tempPlace = (int)Message.charAt(i-1)+ increment;
-				if (tempPlace > 122)
-				{
-					EncryptedMessage +=(char)((int)Message.charAt(i-1)+increment-26);
-				}
-				else
-					EncryptedMessage += (char)((int)Message.charAt(i-1)+increment);
+				EncryptedMessage += Message.charAt(i-1);
 			}
 			else
 			{
-				EncryptedMessage += Message.charAt(i-1);
-
+				if((Message.charAt(i-1)+2)=='Y')
+					EncryptedMessage += 'A';
+				else if((Message.charAt(i-1)+2)=='Z')
+					EncryptedMessage += 'B';
+				else if((Message.charAt(i-1)+2)=='z')
+					EncryptedMessage += 'b';
+				else if((Message.charAt(i-1)+2)=='y')
+					EncryptedMessage += 'A';
+				else
+				{
+					EncryptedMessage += (char)((int)Message.charAt(i-1)+2);
+				}
+			
 			}
-		}		
-		
+		}
 	}
 	public void printString()
 	{
-		System.out.println("Encrypt " + EncryptNumber +":");
-		System.out.println(EncryptedMessage +"\n");
+		System.out.print("Encrypt " + EncryptNumber +":");
+		System.out.print(EncryptedMessage);
 	}
-	public void rotateTimes()
+	public void rotate13Times()
 	{			
 		getString();
-
-		//for (EncryptNumber = 1; EncryptNumber <= 13; EncryptNumber++)
-		do 
-		{
+		//for (EncryptNumber = 1; EncryptNumber < 13; EncryptNumber++)
+		//{
 			encryptString();
 			printString();
-			Message = EncryptedMessage;
-			EncryptedMessage="";
-			EncryptNumber++;
-		}while(!Message.equals(InitialMessage));
-
+		//}
+		
 	}
-
+	
 }
-
-
